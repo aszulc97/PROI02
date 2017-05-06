07b9605dffbd8f608e5fa2e40d6b00e94d0af1ab
Agszu <agataszulc97@gmail.com>
2017-05-06 16:27:50 +0200

    Node implementation

--- tree.h
+++ tree.h
@@ -1,1 +1,29 @@
+#IFNDEF TREE_H_
+#DEFINE TREE_H_
+#include <iostream>
 
+class Tree
+{
+    public:
+      template <class T>
+      class node
+      {
+          int key;
+          char color // r-red, b-black
+          node * up;
+          node * left;
+          node * right;
+          T data;
+      }
+
+      tree(T content); //constructor
+      ~tree(); //destructor
+
+    private:
+      node * root;
+      int size;
+}
+
+
+
+#ENDIF TREE_H_
